e49562e75ace467e0997b86a164e7187dbce5041
acbart <acbart@vt.edu>
2012-12-06 21:39:16 -0500

    Updates

--- conspyre.py
+++ conspyre.py
@@ -8,140 +8,403 @@ except:
     import simplejson as json
 import fix_json
 
-settings = json.loads(open('conspyre.config','rb').read())
+import requests
 
 conspyre_modules = {}
 
+class ConspyreException(Exception):
+    def __str__(self):
+        return """There was an ambiguous exception that occurred while attempting to communicate with the server."""
+
+class HTTPError(ConspyreException):
+    def __str__(self):
+        return """An HTTP error occurred. Please check the server to confirm that it is running correctly."""
+        
+class ServerError(ConspyreException):
+    def __str__(self):
+        return """There was an error on the server, check the server's logs for errors."""
+
+class ConnectionError(ConspyreException):
+    def __str__(self):
+        return """A Connection error occurred. Recheck your internet connection and make sure the server is running."""
+
+class Timeout(ConspyreException):
+    def __str__(self):
+        return """The request timed out."""
+
+class UserNotFoundException(ConspyreException):
+    def __str__(self):
+        return """You must log in before you make that call."""
+        
+class LoginException(ConspyreException):
+    def __str__(self):
+        return """You must log in before you make that call."""
+        
+class ModuleNotFoundException(ConspyreException):
+    def __str__(self):
+        return """That module does not fully exist yet, and it could not be created. Please fix it's setup on the server."""
+        
+class PasswordMismatchException(ConspyreException):
+    def __str__(self):
+        return """The username and password do not match."""
+        
+class UsernameExistsException(ConspyreException):
+    def __str__(self):
+        return """That username already exists."""
+        
+class ResetNotApprovedException(ConspyreException):
+    def __str__(self):
+        return """Your teacher hasn't approved your password reset yet."""
+        
+class ResetAlreadyRequestedException(ConspyreException):
+    def __str__(self):
+        return """You have already requested a password reset from this teacher."""
+        
+exceptions = {"PasswordMismatch": PasswordMismatchException,
+              "ModuleNotFound": ModuleNotFoundException,
+              "UserNotFound": UserNotFoundException,
+              "UsernameExists": UsernameExistsException,
+              "ResetNotApproved": ResetNotApprovedException,
+              "ResetAlreadyRequested": ResetAlreadyRequestedException}
+        
 class ConspyreConnection(object):
+    """
+        This is the main class for connecting to a server. Besides the
+        explicitly defined functions, you can make other calls to the server by
+        using conspyre.<module_name>.<method_name>(). All parameters must be
+        named.
+        
+        *connected*
+        *logged_in*
+        *teacher*
+    """
+    
+    def raise_error_from_status(self, request_object):
+        status = request_object.status_code
+        if status == 500:
+            raise HTTPError()
+        else:
+            request_object.raise_for_status()
+    
     def __init__(self):
         self.module = self.__class__.__name__
-        self.student_id = None
-        self.student_name = ""
-        self.student_email = ""
-        self.student_password = ""
-        self.teacher = settings['teacher']
-        self.school = settings['school']
-        self.server = settings['server']
+        self.id = None
+        self.name = ""
+        self.email = ""
+        self.password = ""
+        self.teacher = ""
+        self.server = ""
         self.connected = False
-    
-    def login(self, student_email, student_password):
-        r = requests.get(self.server+"/conspyre/login/", 
-                         params={"email" : student_email,
-                                 "password" : student_password})
-        if r.status_code != 200:
-            r.raise_for_status()
-        r = json.loads(r.content)
-        if r['type'] == "error":
-            raise Exception(r['message'])
-        self.student_email = student_email
-        self.student_password = student_password
-        self.student_name = r['data']['name']
-        self.student_id = int(r['data']['id'])
+        self.logged_in = False
+        self.devel = True
         
-    def reset_password(self, student_email, student_password):
-        r = requests.get(self.server+"/conspyre/reset_password/", 
-                         params={"email" : student_email,
-                                 "password" : student_password})
+    def log(self, message):
+        """
+        Logs a *message* string into the module's log (stored in JSON on the 
+        server). This could be used to easily dump crash logs, for instance.
+        """
+        id = -1 if self.id is None else self.id
+        data = { 'metadata' : {'email'    : self.email,
+                                   'password' : self.password,
+                                   'id'       : id,
+                                   'teacher'  : self.teacher,
+                                   'module'   : self.module,
+                                   'time'     : time.time()
+                                   },
+                     'message'     : message}
+        print data
+        try:
+            r = requests.post(self.server+'/logging/'+self.module,
+                              data = json.dumps(data))
+        except requests.exceptions.ConnectionError, ce:
+            raise ConnectionError()
         if r.status_code != 200:
             r.raise_for_status()
         r = json.loads(r.content)
         if r['type'] == "error":
-            raise Exception(r['message'])
-        self.student_email = student_email
-        self.student_password = student_password
-        self.student_name = r['data']['name']
-        self.student_id = int(r['data']['id'])
+            raise Exception(r['error'])
     
-    def register(self, student_email, student_name, student_password):
-        r = requests.get(self.server+"/conspyre/register/", 
-                         params={"email" : student_email,
-                                 "name" : student_name,
-                                 "password" : student_password})
+    def login(self, username, password, teacher_id = None):
+        """
+        Logs the user named by the *username* into the server using the
+        *password*. This method must be called before any methods that 
+        assume a user.
+        
+        Optionally, a teacher's id can also be passed in. This will create
+        a Teaching association between this student and teacher.
+        
+        """
+        params={"email" : username,
+                    "password" : password}
+        if password is not None:
+            params['teacher'] = teacher_id;
+        try:
+            r = requests.get(self.server+"/conspyre/login/", 
+                            params=params)
+        except requests.exceptions.ConnectionError, ce:
+            raise ConnectionError()
+        except requests.exceptions.Timeout, to:
+            raise Timeout()
+        except requests.exceptions.HTTPError, he:
+            raise HTTPError()
+        if r.status_code != 200:
+            self.raise_error_from_status(r);
+        r = json.loads(r.content)
+        if r['type'] == "error":
+            if r['error'] in exceptions:
+                raise exceptions(r['error'])
+            else:
+                raise Exception(r['error'])
+        self.email = username
+        self.password = password
+        self.name = r['data']['name']
+        self.id = int(r['data']['id'])
+        self.logged_in = True
+        
+    def reset_password(self, username, password):
+        """
+        Changes *username*'s password to *password*. After this function is called, 
+        the user will also be logged in.
+        
+        Does not require login.
+        """
+        params={"email" : username,
+                    "password" : password}
+        try:
+            r = requests.get(self.server+"/conspyre/reset_password/", 
+                            params=params)
+        except requests.exceptions.ConnectionError, ce:
+            raise ConnectionError()
+        except requests.exceptions.Timeout, to:
+            raise Timeout()
+        except requests.exceptions.HTTPError, he:
+            raise HTTPError()
         if r.status_code != 200:
             r.raise_for_status()
         r = json.loads(r.content)
         if r['type'] == "error":
-            raise Exception(r['message'])
-        self.student_email = student_email
-        self.student_password = student_password
-        self.student_name = student_name
-        self.student_id = int(r['data']['id'])
+            if r['error'] in exceptions:
+                raise exceptions(r['error'])
+            else:
+                raise Exception(r['error'])
+        self.email = username
+        self.password = password
+        self.name = r['data']['name']
+        self.id = int(r['data']['id'])
     
-    def request_reset(self, student_email):
-        r = requests.get(self.server+"/conspyre/request_reset/", 
-                         params={"student" : student_email,
-                                 "teacher" : self.teacher})
+    def register(self, username, name, password, teacher_id = None):
+        """
+        Creates a new user with the *username*, *name*, and *password*. After this
+        function is called, the user will also be logged in.
+        
+        Optionally, a teacher's id can also be passed in. This will create
+        a Teaching association between this student and teacher.
+        
+        Does not require login.
+        """
+        params={"email" : username,
+                    "name" : name,
+                    "password" : password}
+        if password is not None:
+            params['teacher'] = teacher_id;
+        try:
+            r = requests.get(self.server+"/conspyre/register/", 
+                             params= params)
+        except requests.exceptions.ConnectionError, ce:
+            raise ConnectionError()
+        except requests.exceptions.Timeout, to:
+            raise Timeout()
+        except requests.exceptions.HTTPError, he:
+            raise HTTPError()
         if r.status_code != 200:
             r.raise_for_status()
         r = json.loads(r.content)
         if r['type'] == "error":
-            raise Exception(r['message'])
+            if r['error'] in exceptions:
+                raise exceptions(r['error'])
+            else:
+                raise Exception(r['error'])
+        self.email = username
+        self.password = password
+        self.name = name
+        self.id = int(r['data']['id'])
+    
+    def request_reset(self, username):
+        """
+        Sends a request to the current teacher to reset the password for
+        *username*.
+        
+        Does not require login.
+        """
+        try:
+            r = requests.get(self.server+"/conspyre/request_reset/", 
+                            params={"student" : username,
+                                    "teacher" : self.teacher})
+        except requests.exceptions.ConnectionError, ce:
+            raise ConnectionError()
+        except requests.exceptions.Timeout, to:
+            raise Timeout()
+        except requests.exceptions.HTTPError, he:
+            raise HTTPError()
+        if r.status_code != 200:
+            r.raise_for_status()
+        r = json.loads(r.content)
+        if r['type'] == "error":
+            if r['error'] in exceptions:
+                raise exceptions(r['error'])
+            else:
+                raise Exception(r['error'])
             
-    def password_is_resetable(self, student_email):
-        r = requests.get(self.server+"/conspyre/password_resetable/", 
-                         params={"student" : student_email})
+    def password_is_resetable(self, username):
+        """
+        Returns whether the current teacher has approved a reset for *username*.
+        
+        Does not require login.
+        """
+        try:
+            r = requests.get(self.server+"/conspyre/password_resetable/", 
+                            params={"student" : username})
+        except requests.exceptions.ConnectionError, ce:
+            raise ConnectionError()
+        except requests.exceptions.Timeout, to:
+            raise Timeout()
+        except requests.exceptions.HTTPError, he:
+            raise HTTPError()
         if r.status_code != 200:
             r.raise_for_status()
         r = json.loads(r.content)
         return r['data']['status']
         
     def logout(self):
-        self.student_id = None
-        self.student_name = ""
-        self.student_email = ""
-        self.student_password = ""
+        """
+        Logs the currently logged in user out of the system, most likely so that
+        another user can log in. This does not actually communicate with the
+        server.
+        """
+        if not self.logged_in:
+            raise LoginException()
+        self.id = None
+        self.name = ""
+        self.email = ""
+        self.password = ""
+        self.logged_in = False
         
     def get_teacher_list(self):
-        r = requests.get(self.server+"/conspyre/teacher_list/")
+        """
+        Returns a list of all the teachers. This will be a list of dictionaries
+        with the keys "id" (a number that uniquely identifies a teacher), "name"
+        (a name that children will recognize as their teacher), and "username"
+        (a unique name that identifies them, but will probably not be
+        recognizeable to a child).
+        
+        Does not require login.
+        """
+        try:
+            r = requests.get(self.server+"/conspyre/teacher_list/")
+        except requests.exceptions.ConnectionError, ce:
+            raise ConnectionError()
+        except requests.exceptions.Timeout, to:
+            raise Timeout()
+        except requests.exceptions.HTTPError, he:
+            raise HTTPError()
         if r.status_code != 200:
             r.raise_for_status()
         r = json.loads(r.content)
         if r['type'] == "error":
-            raise Exception(r['message'])
+            if r['error'] in exceptions:
+                raise exceptions(r['error'])
+            else:
+                raise Exception(r['error'])
         return r['data']['teachers']
     
     def get_student_list(self, teacher):
+        """
+        Returns a list of all the students associated with a specific teacher.
+        This will be a list of dictionaries with the keys "id" (a number that
+        uniquely identifies a student), "username" (a unique name that 
+        identifies them and hopefully they've memorized), and a "name" (a name
+        that they will recognize as their own but may not be unique).
+        
+        Does not require login.
+        """
         if teacher is None:
             teacher = ""
-        r = requests.get(self.server+"/conspyre/student_list/",
-                         params={"teacher" : teacher})
+        try:
+            r = requests.get(self.server+"/conspyre/student_list/",
+                            params={"teacher" : teacher})
+        except requests.exceptions.ConnectionError, ce:
+            raise ConnectionError()
+        except requests.exceptions.Timeout, to:
+            raise Timeout()
+        except requests.exceptions.HTTPError, he:
+            raise HTTPError()
         if r.status_code != 200:
             r.raise_for_status()
         r = json.loads(r.content)
         if r['type'] == "error":
-            raise Exception(r['message'])
+            if r['error'] in exceptions:
+                raise exceptions(r['error'])
+            else:
+                raise Exception(r['error'])
         return r['data']['students']
         
-    def connect(self, server = None):
-        if server is not None:
-            self.server = server
-        r = requests.get(self.server+"/test/"+self.module)
+    def connect(self, server = 'http://127.0.0.1:8080', devel=True):
+        """
+        Establishes a connection to the *server*. This function must be called
+        before any others.
+        """
+        self.server = server
+        self.devel = devel
+        try:
+            r = requests.get(self.server+"/test/"+self.module)
+        except requests.exceptions.ConnectionError, ce:
+            raise ConnectionError()
+        except requests.exceptions.Timeout, to:
+            raise Timeout()
+        except requests.exceptions.HTTPError, he:
+            raise HTTPError()
         if r.status_code != 200:
-            r.raise_for_status()
+            return self.raise_error_from_status(r)
         r = json.loads(r.content)
         if r['type'] == "error":
-            raise Exception(r['message'])
+            if r['error'] in exceptions:
+                raise exceptions(r['error'])
+            else:
+                raise Exception(r['error'])
         self.connected = True
+        
+    def disconnect(self):
+        """
+        Disconnects from the server. This does not communicate with the server.
+        """
+        self.connected = False
         
     def _make_server_command(self, command):
         def _server_command(**kwargs):
-            metadata = { 'metadata' : {'email'    : self.student_email,
-                                       'password' : self.student_password,
-                                       'id'       : self.student_id,
+            metadata = { 'metadata' : {'email'    : self.email,
+                                       'password' : self.password,
+                                       'id'       : self.id,
                                        'teacher'  : self.teacher,
-                                       'school'   : self.school,
                                        'module'   : self.module,
                                        'time'     : time.time(),
                                        'command'  : command},
                          'data'     : dict(kwargs)
                      }
-            assert self.connected, "You are not connected; cannot call command %s." % command
+            if not self.logged_in:
+                raise LoginException()
             r = requests.post(self.server+'/modules/'+self.module+'/'+command,
                               data = json.dumps(metadata))
-            if r != 200:
-                r.raise_for_status()
+            if r.status_code != 200:
+                return self.raise_error_from_status(r)
             r = json.loads(r.content)
-            return r['data']
+            if r['type'] == 'error':
+                raise Exception(r['error'], r['data'])
+            if len(r['data']) == 0:
+                return None
+            if len(r['data']) == 1:
+                return r['data'][0]
+            else:
+                return r['data']
         return _server_command
     
     def __getattr__(self, name):
--- test_conspyre.py
+++ test_conspyre.py
@@ -1,49 +1,91 @@
+import sys
 import conspyre
 import time
-
-conspyre.example.connect()
+from conspyre import ConnectionError, ModuleNotFoundException
 
 try:
+    conspyre.template.connect(server= 'http://127.0.0.1:8080')
+except Exception, me:
+    print me
+    print "Skipping creation"
+
+conspyre.template.login('Template_dog_lover', 'pass')
+
+conspyre.template.put(key="points", value=100)
+print conspyre.template.get(key="points")
+print conspyre.template.has(key="points")
+conspyre.template.update(dict={"points":101, "title":"Lord Awesome"})
+print conspyre.template.items()
+
+print conspyre.template.has(key="points")
+print conspyre.template.items()
+print conspyre.template.put(key="points", value=100)
+print conspyre.template.get(key="points")
+print conspyre.template.has(key="clothes")
+print conspyre.template.update(pairs={"points":200, "clothes": "Alpha, Beta"})
+print conspyre.template.get(key="points")
+print conspyre.template.items()
+conspyre.template.logout()
+
+sys.exit()
+    
+print "Testing Logging"
+conspyre.template.log("Exploding!")
+conspyre.template.log("Another!!")
+conspyre.template.log("Puppies!")
+conspyre.template.disconnect()
+
+print "Attempting to connect to the school server."
+conspyre.template.connect(server= 'http://127.0.0.1:8080', school= 'Chester Community Charter School')
+    
+try:
     print "Registering Becky"
-    conspyre.example.register('rtrexler', 'Becky Trexler', 'pass')
+    conspyre.template.register('rtrexler', 'Becky Trexler', 'pass')
+except ConnectionError, c:
+    print c
 except Exception, e:
     print e
 
 print "Logging Becky in and out"
-conspyre.example.login('rtrexler', 'pass')
-conspyre.example.logout()
+conspyre.template.login('rtrexler', 'pass')
+conspyre.template.logout()
 
 print "Viewing Teachers"
-print conspyre.example.get_teacher_list()
+print conspyre.template.get_teacher_list()
 
 print "Setting Teacher to Cory"
-conspyre.example.teacher= 'acbart'
+conspyre.template.teacher= 'acbart'
 
 print "Viewing Students"
-print conspyre.example.get_student_list(None)
+print conspyre.template.get_student_list(None)
 
 print "Requesting Password Reset"
 try:
-    conspyre.example.request_reset('rtrexler')
+    conspyre.template.request_reset('rtrexler')
 except Exception, e:
     print e
     print "Skipping password reset."
 
-while not conspyre.example.password_is_resetable('rtrexler'):
-    print "Waiting for password to be reset..."
+attempts_left = 5
+print "Waiting for password to be reset..."
+print "(Log into the server to reset the password)"
+while not conspyre.template.password_is_resetable('rtrexler') and attempts_left:
     time.sleep(4)
-
-conspyre.example.reset_password('rtrexler', 'password')
+    attempts_left-= 1
+    print "... Password still not reset. Trying %d more times." % attempts_left
+if attempts_left:
+    pass
+    #conspyre.template.reset_password('rtrexler', 'password')
     
-conspyre.example.login('dog_lover', 'pass')
+conspyre.template.login('dog_lover', 'pass')
 
-print "Welcome %s!" % conspyre.example.student_name
-print "First", conspyre.example.get_dog_list()['dogs']
-dogs = conspyre.example.get_dog_list()['dogs']
-conspyre.example.rename_dog(id=dogs[0]['id'], name='Alfred')
-print "Renamed", conspyre.example.get_dog_list()['dogs']
-conspyre.example.add_dog(breed='poodle', name='dogbert')
-print "Added", conspyre.example.get_dog_list()['dogs']
-dogs = conspyre.example.get_dog_list()['dogs']
-conspyre.example.release_dog(id=dogs[0]['id'])
-print "Removed", conspyre.example.get_dog_list()['dogs']
+print "Welcome %s!" % conspyre.template.student_name
+print "First", conspyre.template.get_dog_list()['dogs']
+dogs = conspyre.template.get_dog_list()['dogs']
+conspyre.template.rename_dog(id=dogs[0]['id'], name='Alfred')
+print "Renamed", conspyre.template.get_dog_list()['dogs']
+conspyre.template.add_dog(breed='poodle', name='dogbert')
+print "Added", conspyre.template.get_dog_list()['dogs']
+dogs = conspyre.template.get_dog_list()['dogs']
+conspyre.template.release_dog(id=dogs[0]['id'])
+print "Removed", conspyre.template.get_dog_list()['dogs']
